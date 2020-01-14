1a5269f9e4af9c224e4b0e38d34005844ff63013
Christopher Hiller <boneskull@boneskull.com>
2020-01-13 16:55:14 -0800

    multiple async done() calls result in failure; closes #4151
    - added a method in `errors` module to create a "multiple done" err
    - modernize `multiple-done.spec.js`
    - refactor errors into constants in `errors` module
    - remove `Runner#started` prop; replace with `Runner#state` prop + constants
    - add a catchall `createFatalError()` function to `errors` module; this is called
      when a test fails twice by other means (unsure what those means are yet)
    - force color in Travis CI b/c my eyes

--- test/integration/multiple-done.spec.js
+++ test/integration/multiple-done.spec.js
@@ -1,50 +1,51 @@
 'use strict';
 
-var assert = require('assert');
-var run = require('./helpers').runMochaJSON;
-var args = [];
+var runMochaJSON = require('./helpers').runMochaJSON;
+var invokeMocha = require('./helpers').invokeMocha;
+var ERR_MOCHA_MULTIPLE_DONE = require('../../lib/errors').constants
+  .ERR_MOCHA_MULTIPLE_DONE;
 
 describe('multiple calls to done()', function() {
   var res;
   describe('from a spec', function() {
     before(function(done) {
-      run('multiple-done.fixture.js', args, function(err, result) {
+      runMochaJSON('multiple-done', function(err, result) {
         res = result;
         done(err);
       });
     });
 
-    it('results in failures', function() {
-      assert.strictEqual(res.stats.pending, 0, 'wrong "pending" count');
-      assert.strictEqual(res.stats.passes, 1, 'wrong "passes" count');
-      assert.strictEqual(res.stats.failures, 1, 'wrong "failures" count');
+    it('results in failure', function() {
+      expect(res, 'to have failed test count', 1)
+        .and('to have passed test count', 1)
+        .and('to have pending test count', 0)
+        .and('to have failed');
     });
 
     it('throws a descriptive error', function() {
-      assert.strictEqual(
-        res.failures[0].err.message,
-        'done() called multiple times'
-      );
+      expect(res, 'to have failed with error', 'done() called multiple times');
     });
   });
 
   describe('with error passed on second call', function() {
     before(function(done) {
-      run('multiple-done-with-error.fixture.js', args, function(err, result) {
+      runMochaJSON('multiple-done-with-error', function(err, result) {
         res = result;
         done(err);
       });
     });
 
-    it('results in failures', function() {
-      assert.strictEqual(res.stats.pending, 0, 'wrong "pending" count');
-      assert.strictEqual(res.stats.passes, 1, 'wrong "passes" count');
-      assert.strictEqual(res.stats.failures, 1, 'wrong "failures" count');
+    it('results in failure', function() {
+      expect(res, 'to have failed test count', 1)
+        .and('to have passed test count', 1)
+        .and('to have pending test count', 0)
+        .and('to have failed');
     });
 
     it('should throw a descriptive error', function() {
-      assert.strictEqual(
-        res.failures[0].err.message,
+      expect(
+        res,
+        'to have failed with error',
         "second error (and Mocha's done() called multiple times)"
       );
     });
@@ -52,78 +53,119 @@ describe('multiple calls to done()', function() {
 
   describe('with multiple specs', function() {
     before(function(done) {
-      run('multiple-done-specs.fixture.js', args, function(err, result) {
+      runMochaJSON('multiple-done-specs', function(err, result) {
         res = result;
         done(err);
       });
     });
 
-    it('results in a failure', function() {
-      assert.strictEqual(res.stats.pending, 0);
-      assert.strictEqual(res.stats.passes, 2);
-      assert.strictEqual(res.stats.failures, 1);
-      assert.strictEqual(res.code, 1);
+    it('results in failure', function() {
+      expect(res, 'to have failed test count', 1)
+        .and('to have passed test count', 2)
+        .and('to have pending test count', 0)
+        .and('to have failed');
     });
 
     it('correctly attributes the error', function() {
-      assert.strictEqual(res.failures[0].fullTitle, 'suite test1');
-      assert.strictEqual(
-        res.failures[0].err.message,
-        'done() called multiple times'
-      );
+      expect(res.failures[0], 'to satisfy', {
+        fullTitle: 'suite test1',
+        err: {
+          message: 'done() called multiple times'
+        }
+      });
     });
   });
 
   describe('from a before hook', function() {
     before(function(done) {
-      run('multiple-done-before.fixture.js', args, function(err, result) {
+      runMochaJSON('multiple-done-before', function(err, result) {
         res = result;
         done(err);
       });
     });
 
-    it('results in a failure', function() {
-      assert.strictEqual(res.stats.pending, 0);
-      assert.strictEqual(res.stats.passes, 1);
-      assert.strictEqual(res.stats.failures, 1);
-      assert.strictEqual(res.code, 1);
+    it('results in failure', function() {
+      expect(res, 'to have failed test count', 1)
+        .and('to have passed test count', 1)
+        .and('to have pending test count', 0)
+        .and('to have failed');
     });
 
     it('correctly attributes the error', function() {
-      assert.strictEqual(
-        res.failures[0].fullTitle,
-        'suite "before all" hook in "suite"'
-      );
-      assert.strictEqual(
-        res.failures[0].err.message,
-        'done() called multiple times'
-      );
+      expect(res.failures[0], 'to satisfy', {
+        fullTitle: 'suite "before all" hook in "suite"',
+        err: {
+          message: 'done() called multiple times'
+        }
+      });
     });
   });
 
   describe('from a beforeEach hook', function() {
     before(function(done) {
-      run('multiple-done-beforeEach.fixture.js', args, function(err, result) {
+      runMochaJSON('multiple-done-beforeEach', function(err, result) {
         res = result;
         done(err);
       });
     });
 
     it('results in a failure', function() {
-      assert.strictEqual(res.stats.pending, 0);
-      assert.strictEqual(res.stats.passes, 2);
-      assert.strictEqual(res.stats.failures, 2);
-      assert.strictEqual(res.code, 2);
+      expect(res, 'to have failed test count', 2)
+        .and('to have passed test count', 2)
+        .and('to have pending test count', 0)
+        .and('to have exit code', 2);
     });
 
     it('correctly attributes the errors', function() {
-      assert.strictEqual(res.failures.length, 2);
-      res.failures.forEach(function(failure) {
-        assert.strictEqual(
-          failure.fullTitle,
-          'suite "before each" hook in "suite"'
-        );
-        assert.strictEqual(failure.err.message, 'done() called multiple times');
+      expect(res.failures, 'to satisfy', [
+        {
+          fullTitle: 'suite "before each" hook in "suite"',
+          err: {message: 'done() called multiple times'}
+        },
+        {
+          fullTitle: 'suite "before each" hook in "suite"',
+          err: {message: 'done() called multiple times'}
+        }
+      ]);
+    });
+  });
+
+  describe('when done() called asynchronously', function() {
+    before(function(done) {
+      invokeMocha(
+        require.resolve('./fixtures/multiple-done-async.fixture.js'),
+        function(err, result) {
+          res = result;
+          done(err);
+        },
+        'pipe'
+      );
+    });
+
+    it('results in error', function() {
+      expect(res, 'to satisfy', {
+        code: expect.it('to be greater than', 0),
+        output: /done\(\) called multiple times/
+      });
+    });
+
+    it('fail with an error containing the information about the test', function() {
+      expect(res.output, 'to match', /should fail in an async test case/);
+    });
+
+    describe('when errored after Runner has completed', function() {
+      // WARNING: non-deterministic!
+      before(function() {
+        if (/1\) should fail in an async test case/.test(res.output)) {
+          return this.skip();
+        }
+      });
+
+      it('should provide extra information about the Runnable', function() {
+        expect(res.output, 'to match', /multiple-done-async\.fixture\.js/)
+          .and('to match', /type: 'test'/)
+          .and('to match', /body: 'function/)
+          .and('to match', new RegExp(ERR_MOCHA_MULTIPLE_DONE));
       });
     });
   });
--- test/unit/runner.spec.js
+++ test/unit/runner.spec.js
@@ -14,6 +14,9 @@ var EVENT_TEST_FAIL = Runner.constants.EVENT_TEST_FAIL;
 var EVENT_TEST_RETRY = Runner.constants.EVENT_TEST_RETRY;
 var EVENT_RUN_END = Runner.constants.EVENT_RUN_END;
 var STATE_FAILED = Runnable.constants.STATE_FAILED;
+var STATE_IDLE = Runner.constants.STATE_IDLE;
+var STATE_RUNNING = Runner.constants.STATE_RUNNING;
+var STATE_STOPPED = Runner.constants.STATE_STOPPED;
 
 describe('Runner', function() {
   var sandbox;
@@ -743,7 +746,7 @@ describe('Runner', function() {
 
           describe('when Runner has already started', function() {
             beforeEach(function() {
-              runner.started = true;
+              runner.state = STATE_RUNNING;
             });
 
             it('should not emit start/end events', function() {
@@ -758,20 +761,39 @@ describe('Runner', function() {
             });
           });
 
-          describe('when Runner has not already started', function() {
-            beforeEach(function() {
-              runner.started = false;
+          describe('when Runner not running', function() {
+            describe('when idle', function() {
+              beforeEach(function() {
+                runner.state = STATE_IDLE;
+              });
+
+              it('should emit start/end events for the benefit of reporters', function() {
+                expect(
+                  function() {
+                    runner.uncaught(err);
+                  },
+                  'to emit from',
+                  runner,
+                  'start'
+                ).and('to emit from', runner, 'end');
+              });
             });
 
-            it('should emit start/end events for the benefit of reporters', function() {
-              expect(
-                function() {
-                  runner.uncaught(err);
-                },
-                'to emit from',
-                runner,
-                'start'
-              ).and('to emit from', runner, 'end');
+            describe('when stopped', function() {
+              beforeEach(function() {
+                runner.state = STATE_STOPPED;
+              });
+
+              it('should emit start/end events for the benefit of reporters', function() {
+                expect(
+                  function() {
+                    runner.uncaught(err);
+                  },
+                  'to emit from',
+                  runner,
+                  'start'
+                ).and('to emit from', runner, 'end');
+              });
             });
           });
         });
